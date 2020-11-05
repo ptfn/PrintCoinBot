9431372e1a52aa48c1efd8033ccfabc9bd5882a7
D34D_FR13ND5 <47918260+ptfn@users.noreply.github.com>
2020-11-05 20:12:23 +0700

    Update bot_coin.py

--- bot_coin.py
+++ bot_coin.py
@@ -5,7 +5,6 @@ r = requests.get('https://www.bw.com/exchange/config/controller/website/pricecon
 data = r.json()
 coins = ['btc','eth','xmr','grin']
 
-
 def price_coin(arr):
     string = ''
     for i in range(len(arr)):
@@ -19,11 +18,10 @@ def lists_coin(arr):
         string = string + arr[i] + '\n'
     return  string
 
-
 @bot.message_handler(commands=['start'])
 def add_coin(message):
     bot.send_message(message.chat.id,'This bot is for sending the price of a cryptocurrency. It sends it at 10, 14, 18 and 22 hours. List coin:btc, eth, xmr.')
-
+    
     while True:
         time = datetime.datetime.now()
         if time.hour == 10 and time.minute == 00 and time.second == 00 and time.microsecond == 000000:
@@ -36,5 +34,5 @@ def add_coin(message):
             bot.send_message(message.chat.id, 'Coins:\n' + price_coin(coins))
         elif time.hour == 22 and time.minute == 00 and time.second == 00 and time.microsecond == 000000:
             bot.send_message(message.chat.id, 'Coins:\n' + price_coin(coins))
-        
+            
 bot.polling()
